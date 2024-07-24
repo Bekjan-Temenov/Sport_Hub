128ed6473f26b76b6d41e3605c3b0c641408999b
Aizhanat <binaainy@gmail.com>
2024-07-24 21:02:40 +0600

    sports

--- src/features/Home/ui/Home.jsx
+++ src/features/Home/ui/Home.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Container from "../../../shared/helpers/Container";
 import SliderHome from "./SliderHome";
+import HomeSports from "./HomeSports";
+
 
 
 function Home() {
   return (
-  
-      <SliderHome />
+     <>
+    <SliderHome />
+    <HomeSports/>
+     </>
+    
    
   );
 }
