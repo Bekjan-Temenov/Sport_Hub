18729b125e86c3ecfa397d4b48eb6fd103be54aa
Aizhanat <binaainy@gmail.com>
2024-08-03 15:24:05 +0600

    register

--- src/app/router/index.jsx
+++ src/app/router/index.jsx
@@ -2,14 +2,17 @@ import { useRoutes } from "react-router-dom";
 import Layout from "../../widgets/Layout/Layout";
 import HomeRoute from "../../features/Home/route";
 import Home from "../../features/Home/ui/Home";
+import SignUpRoute from "../../features/Auth/SignUp/route";
+
 
 const MyRoutes = () => {
     return useRoutes([
         {
             path:"",
             element:<Layout/>,
-            children: [{ path: '', element: <Home/> } ,HomeRoute ],
-        }
+            children: [{ path: '', element: <Home/> } ,HomeRoute   ],
+        },
+       SignUpRoute
     ])
 }
 export default MyRoutes
---
+++ src/features/Auth/SignUp/route.jsx
@@ -0,0 +1,9 @@
+import SignUp from "./ui/SignUp";
+
+const SignUpRoute = {
+    path: "signup",
+    element: <SignUp />,
+    
+};
+
+export default SignUpRoute;
