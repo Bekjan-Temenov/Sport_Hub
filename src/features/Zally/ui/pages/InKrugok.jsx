f8b28a3d020b4320deb232da133f1ddf4a948d46
TM <temenovbekzan@gmail.com>
2024-08-29 14:49:16 +0600

    добавлено анимация

--- src/features/Zally/ui/pages/InKrugok.jsx
+++ src/features/Zally/ui/pages/InKrugok.jsx
@@ -1,69 +1,186 @@
+// import React from "react";
+// import { Link, useLocation } from "react-router-dom";
+// import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+// import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+// import Container from "../../../../shared/helpers/Container";
+// import ModalKrugok from "./modalKrugok";
+
+// const zally = [
+//   "Билимкана арена",
+//   "Afb баскетбольный клуб",
+//   "Kyrgyzbasketball",
+//   "3x3Kyrgyzstan",
+
+// ];
+
+// function InKrugok() {
+//   const location = useLocation();
+//   const queryParams = new URLSearchParams(location.search);
+//   const sport = JSON.parse(decodeURIComponent(queryParams.get("sport")));
+
+//   if (!sport) {
+//     return <p>Data not found</p>;
+//   }
+//   return (
+//     <Container>
+//       {sport ? (
+//         <div>
+//           <div
+//             className="relative my-8 text-white flex items-center bg-cover mx-auto rounded-r-full justify-between w-[90%] rounded-l-md md:pl-[40px] pl-[20px] border-2 border-white"
+//             style={{
+//               backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${sport.inbackgroundImage})`,
+//             }}
+//           >
+//             <div className="flex flex-col   hidden lg:block w-[40%]">
+//               {zally.map((item, index) => (
+//                 <div key={index} className="flex items-center justify-between mt-2 ">
+//                   <span className="text-lg font-medium text-white border-b">
+//                     {item}
+//                   </span>
+//                   <Link to="/arena">
+//                     <button className="flex items-center justify-center w-8 h-8 ml-4 bg-red-600 rounded-md">
+//                       <ArrowForwardIcon />
+//                     </button>
+//                   </Link>
+//                 </div>
+//               ))}
+//             </div>
+//             <Link to={`/zally/${sport.id}`}>
+//               <button className="flex items-center justify-center w-8 h-8 bg-red-600 rounded-md">
+//                 <ArrowBackIcon />
+//               </button>
+//             </Link>
+//             <h1 className="block text-2xl md:hidden">{sport.name}</h1>
+//             <img className="invisible w-[21%]" src={sport.boll} />
+//             <img
+//               className="absolute lg:w-[22%] w-[19%] sm:w-[20%] md:w-[21%] right-[-3%]"
+//               src={sport.boll}
+//               alt="basketball"
+//             />
+//           </div>
+//           <ModalKrugok zally={zally}/>
+//         </div>
+//       ) : (
+//         <p className="text-white">Loading...</p>
+//       )}
+//     </Container>
+//   );
+// }
+// export default InKrugok;
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import Container from "../../../../shared/helpers/Container";
-import ModalKrugok from "./modalKrugok";
+import { Link } from "react-router-dom";
 
 const zally = [
-  "Билимкана арена",
+  "EL CLASICO",
+  "Arsenal",
   "Afb баскетбольный клуб",
+  "Bilimkana Arena»",
+  "SEYTEK SPORT",
+  "Sun City",
+  "Arsenal",
   "Kyrgyzbasketball",
-  "3x3Kyrgyzstan",
-  
 ];
 
-function InKrugok() {
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const sport = JSON.parse(decodeURIComponent(queryParams.get("sport")));
-
-  if (!sport) {
-    return <p>Data not found</p>;
-  }
+function InKrugok({ sport, isOpen, handleOpenClick }) {
+  
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.3,
+      },
+    },
+  };
+  
   return (
-    <Container>
-      {sport ? (
-        <div>
-          <div
-            className="relative my-8 text-white flex items-center bg-cover mx-auto rounded-r-full justify-between w-[90%] rounded-l-md md:pl-[40px] pl-[20px] border-2 border-white"
-            style={{
-              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${sport.inbackgroundImage})`,
-            }}
+    <motion.div
+      className={`relative flex items-center bg-cover mx-auto justify-between w-[90%]  md:pr-[40px] pr-[20px] border-2 border-white ${
+        isOpen ? " rounded-r-full " : " rounded-l-full" 
+      }`}
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${isOpen ? sport.inbackgroundImage : sport.backgroundImage2})`,
+      }}  
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      <motion.img
+        animate={{ x: isOpen ? 900 : 0 }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+        className="absolute lg:w-[22%] w-[19%] sm:w-[20%] md:w-[21%] left-[-3%]"
+        src={sport.boll}
+        alt="basketball"
+      />
+      <motion.img
+        animate={{ x: isOpen ? 900 : 0 }}
+        transition={{ duration: 0.5 }}
+        className={`w-[21%]   ${isOpen ? "hidden" :"invisible"}`}
+        src={sport.boll}
+      />
+      {isOpen && (
+        <div div className="flex items-center justify-between w-full ">
+          <motion.div
+            variants={containerVariants}
+            initial="hidden"
+            animate={{ opacity: isOpen ? 1 : 0 }}
+            transition={{ duration: 1.5 }}
+            className="hidden   ml-[68px]  w-[90%] gap-x-[80px] lg:grid lg:grid-cols-2 lg:block "
           >
-            <div className="flex flex-col   hidden lg:block w-[40%]">
-              {zally.map((item, index) => (
-                <div key={index} className="flex items-center justify-between mt-2 ">
-                  <span className="text-lg font-medium text-white border-b">
-                    {item}
-                  </span>
-                  <Link to="/arena">
-                    <button className="flex items-center justify-center w-8 h-8 ml-4 bg-red-600 rounded-md">
-                      <ArrowForwardIcon />
-                    </button>
-                  </Link>
-                </div>
-              ))}
-            </div>
-            <Link to={`/zally/${sport.id}`}>
-              <button className="flex items-center justify-center w-8 h-8 bg-red-600 rounded-md">
-                <ArrowBackIcon />
-              </button>
-            </Link>
-            <h1 className="block text-2xl md:hidden">{sport.name}</h1>
-            <img className="invisible w-[21%]" src={sport.boll} />
-            <img
-              className="absolute lg:w-[22%] w-[19%] sm:w-[20%] md:w-[21%] right-[-3%]"
-              src={sport.boll}
-              alt="basketball"
-            />
-          </div>
-          <ModalKrugok zally={zally}/>
+            {zally.map((item, index) => (
+              <div
+                key={index}
+                className="flex items-center justify-between mt-2 "
+              >
+                <span className="font-medium text-white border-b text-md xl:text-lg lg:text-md">
+                  {item}
+                </span>
+                <Link to="/arena">
+                  <button>
+                    <ArrowForwardIcon className="flex items-center justify-center ml-4 bg-[#FE0404] rounded-md" />
+                  </button>
+                </Link>
+              </div>
+            ))}
+          </motion.div>
+          <button 
+          
+          className="flex items-center justify-center p-3 bg-[#FE0404] ml-[20px] rounded-md">
+            <ArrowBackIcon />
+          </button>
         </div>
-      ) : (
-        <p className="text-white">Loading...</p>
       )}
-    </Container>
+      <motion.h1
+        animate={{ opacity: isOpen ? 0 : 1 }}
+        transition={{ duration: 0.3 }}
+        className={`text-xl sm:text-4xl  md:text-6xl ${
+          isOpen ? "hidden" : "block"
+        }`}
+      >
+        Кружки
+      </motion.h1>
+      <motion.button
+        animate={{ opacity: isOpen ? 0 : 1 }}
+        transition={{ duration: 0.5 }}
+        onClick={handleOpenClick}
+        className={`p-5 text-sm  text-center bg-[#FE0404] rounded-md hover:bg-red-700 sm:text-md md:text-2xl ${
+          isOpen ? "hidden" : "block"
+        }`}
+      >
+        Открыть
+      </motion.button>
+      <motion.img
+        animate={{ x: isOpen ? 900 : 0 }}
+        transition={{ duration: 0.5 }}
+        className={`w-[21%]   ${isOpen ? "invisible" :"hidden"}`}
+        src={sport.boll}
+      />
+    </motion.div>
   );
 }
+
 export default InKrugok;
--- src/features/Zally/ui/pages/Zally.jsx
+++ src/features/Zally/ui/pages/Zally.jsx
@@ -1,68 +1,36 @@
 import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import { Link, useParams } from "react-router-dom";
 import Container from "../../../../shared/helpers/Container";
 import { sports } from "../../../../shared/api/api";
+import InZally from "./InZally";
+import InKrugok from "./InKrugok";
 
 function Zally() {
   const { id } = useParams();
   const [sport, setSport] = useState();
+  const [isClicked, setIsClicked] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     const findSport = sports.find((data) => data.id === parseInt(id));
     setSport(findSport || null);
   }, [id]);
-  console.log(sport);
+
+  const handleButtonClick = () => {
+    setIsClicked(true);
+  };
+  const handleOpenClick = () => {
+    setIsOpen(true);
+  };
+
   return (
     <Container>
       {sport ? (
-        <div  className="flex flex-col gap-[40px] text-white mb-[100px]">
+        <div className="flex flex-col gap-[40px] text-white mb-[100px]">
           <li className="text-4xl">{sport.name}</li>
-          <div
-            className="relative  flex items-center bg-cover  mx-auto rounded-l-full justify-between w-[90%]  rounded-r-md md:pr-[40px] pr-[20px]   border-2 border-white"
-            style={{
-              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)) ,url(${sport.backgroundImage})`,
-            }}
-          >
-            <img
-              className=" absolute lg:w-[22%] w-[19%] sm:w-[20%] md:w-[ 21%]  left-[-3%]"
-              src={sport.boll}
-              alt="basketball"
-            />
-            <img className=" invisible w-[21%]" src={sport.boll} />
-            <h1 className="text-xl sm:text-4xl md:text-6xl">Залы</h1>
-            <Link
-              to={`/zally/in-krugok?sport=${encodeURIComponent(
-                JSON.stringify(sport)
-              )}`}
-              className="w-[30%] sm:w-[20%] md:w-[20%] py-1 md:py-3 px-3 text-center bg-red-500 rounded-md hover:bg-red-700 text-sm sm:text-md"
-            >
-              <button >
-                Открыть
-              </button>
-            </Link>
-          </div>
-          <div
-            className="relative  flex items-center bg-cover  mx-auto rounded-l-full justify-between w-[90%]  rounded-r-md md:pr-[40px] pr-[20px]   border-2 border-white"
-            style={{
-              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)) ,url(${sport.backgroundImage2})`,
-            }}
-          >
-            <img
-              className=" absolute lg:w-[22%] w-[19%] sm:w-[20%] md:w-[ 21%]  left-[-3%]"
-              src={sport.boll}
-              alt="basketball"
-            />
-            <img className=" invisible w-[21%]" src={sport.boll} />
-            <h1 className="text-xl sm:text-4xl md:text-6xl">Кружки</h1>
-            <Link
-              to={`/zally/in-zally?sport=${encodeURIComponent(
-                JSON.stringify(sport)
-              )}`}
-              className="w-[30%] sm:w-[20%] md:w-[20%] py-1 md:py-3 px-3 text-center bg-red-500 rounded-md hover:bg-red-700 text-sm sm:text-md"
-            >
-              <button>Открыть</button>
-            </Link>
-          </div>
+          <InZally isClicked={isClicked} handleButtonClick={handleButtonClick} sport={sport}/>
+          <InKrugok handleOpenClick={handleOpenClick} isOpen={isOpen} sport={sport}/>
         </div>
       ) : (
         <p>Loading...</p>
