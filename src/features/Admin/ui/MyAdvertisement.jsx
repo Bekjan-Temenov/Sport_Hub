5af7071147eb4a3efc1a24d282dea67d11f18f2f
Nurhan <nurhan.vip.mmm@gmail.com>
2024-09-07 10:40:00 +0600

    admin

--- src/features/Admin/ui/MyAdvertisement.jsx
+++ src/features/Admin/ui/MyAdvertisement.jsx
@@ -9,6 +9,7 @@ import Setting from "../../../shared/assets/svg/setting.svg"
 import Login from "../../../shared/assets/svg/login-circle.svg"
 import Search from "../../../shared/assets/svg/search-1.svg"
 import Crop from "../../../shared/assets/svg/crop.svg"
+import { GoPlus } from "react-icons/go";
 
 
 const MyAdvertisement = () => {
@@ -123,37 +124,70 @@ const MyAdvertisement = () => {
                     }}>Выйти</p>
                 </button>
             </div>
-            <div style={{
-                width: "957px",
-                height: "42px",
-                margin: "30px 81px 38px 88px",
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-            }}>
-                <div style={{ display: "flex", gap: "15px" }}>
-                    <input style={{
-                        width: "677px",
-                        height: "42px",
-                        borderRadius: "30px",
-                        background: "#E5E4E9",
-                        outline: "none",
-                        padding: "11px 475px 11px 22px",
-                    }} type="text" />
-                    <img src={Search} alt="" style={{ cursor: "pointer" }} />
+            <div>
+                <div style={{
+                    width: "957px",
+                    height: "42px",
+                    margin: "30px 81px 38px 88px",
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                }}>
+                    <div style={{ display: "flex", gap: "15px" }}>
+                        <input style={{
+                            width: "677px",
+                            height: "42px",
+                            borderRadius: "30px",
+                            background: "#E5E4E9",
+                            outline: "none",
+                            padding: "11px 475px 11px 22px",
+                        }} type="text" />
+                        <img src={Search} alt="" style={{ cursor: "pointer" }} />
+                    </div>
+                    <div>
+                        <div style={{ display: "flex", gap: "9px", alignItems: "center" }}>
+                            <p style={{
+                                fontSize: "12px",
+                                fontWeight: "400",
+                                color: "#fff",
+                                fontFamily: "Comfortaa"
+                            }}>Бекенова <br />Сабина</p>
+                            <img src={Crop} alt="" />
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <div style={{ display: "flex", gap: "9px", alignItems: "center" }}>
-                        <p style={{
-                            fontSize: "12px",
-                            fontWeight: "400",
-                            color: "#fff",
-                            fontFamily: "Comfortaa"
-                        }}>Бекенова <br />Сабина</p>
-                        <img src={Crop} alt="" />
+                <div style={{
+                    width: "996px",
+                    height: "auto",
+                    marginTop: "38px",
+                    marginLeft: "49px",
+                    background: "#0C192E7A",
+                    padding:"0px 0px 60px 0px"
+                }}>
+                    <div style={{ display: "flex", justifyContent: "space-between", margin: "0px 40px 55px 40px", width: "916px", height: "56px", padding: "15px 0px 55px 0px", borderBottom: "1.04px solid #B6B7BC" }}>
+
+                        <p style={{ fontWeight: "700", fontSize: "32px", fontFamily: "Lato", color: "#fff" }}>Мои рекламы</p>
+                        <button style={{
+                            width: "204px",
+                            height: "40px",
+                            borderRadius: "8px",
+                            background: "#FE0404",
+                            color: "#fff"
+                        }}>
+                            <p style={{ fontWeight: "600", fontSize: "20px", fontFamily: "Comfortaa", display: "flex", justifyContent: "center", alignItems: "center", gap: "10px" }}><GoPlus />Добавить</p>
+                        </button>
+                    </div>
+                    <div style={{
+                        width: '846px',
+                        height: '395px',
+                        border: '1px solid red',
+                        margin: "0px 40px 60px 40px"
+                    }}>
+                        
                     </div>
                 </div>
             </div>
+
         </div>
     )
 }
